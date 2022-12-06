b3387f6700e98d38be4a503fba4fce2f9d375ba0
Hanna Mandzirokha <hanna.mandzirokha@gmail.com>
2022-12-06 10:09:51 +0100

    set up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,10 +7,19 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
     </div>
   );
 };
+
+//  * Стили компонента App
+//  */
+// .App {
+//   display: grid;
+//   grid-template-columns: 1fr;
+//   grid-gap: 16px;
+//   padding-bottom: 24px;
+// }
