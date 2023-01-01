47f268e03ad088397f7004961ad59f3658742bed
Hanna Mandzirokha <hanna.mandzirokha@gmail.com>
2023-01-01 19:39:30 +0100

    fixed position center

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,14 +1,15 @@
 import { FallingLines } from 'react-loader-spinner';
+import { LoaderWrap } from './Loader.styled';
 
 export const Loader = () => {
   return (
-    <div>
+    <LoaderWrap>
       <FallingLines
         color="#3f51b5"
         width="100"
         visible={true}
         ariaLabel="falling-lines-loading"
       />
-    </div>
+    </LoaderWrap>
   );
 };
